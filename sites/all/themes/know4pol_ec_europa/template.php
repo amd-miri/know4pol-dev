025def637622c1a2879963586587771256334b6c
COUNET Jerome (JRC-EXT) <Jerome.COUNET@ext.ec.europa.eu>
2018-02-28 15:11:29 +0100

    Added temmplates for file content type + preprocess

--- sites/all/themes/know4pol_ec_europa/template.php
+++ sites/all/themes/know4pol_ec_europa/template.php
@@ -42,4 +42,72 @@ function know4pol_ec_europa_menu_link(array $variables) {
 function know4pol_ec_europa_preprocess_node(&$variables, $hook) {
   // For all content types.
   drupal_add_js('https://visualise.jrc.ec.europa.eu/javascripts/api/viz_v1.js', 'external');
+  //drupal_add_js(path_to_theme() . '/assets/scripts/ecl-navigation-inpages.js', array('group' => JS_THEME));
+
+  /* COUNEJE */
+
+  // Node is a file
+  if($variables['type'] == 'file') _preprocess_node_file($variables, $hook);
+}
+  
+function _preprocess_node_file(&$variables, $hook) {
+  $node = $variables['elements']['#node'];
+  // is file a link or a file ?
+  $node->file_link = _preprocess_node_file__field_file($node);
+  // filetype icon
+  $_icon_css = $node->field_file_type[LANGUAGE_NONE][0]['taxonomy_term']->field_file_type_classname[LANGUAGE_NONE];
+  // default icon if no type
+  $node->file_type_css = "." . count($_icon_css)?$_icon_css[0][value]:'ecl-icon--file';
+  
+  
+  // languages versions
+  $translations = translation_node_get_translations($node->nid);
+  foreach ($translations as $t) {
+    // other languages olny
+	if($t->nid != $node->nid) {
+	  $t_node = node_load($t->nid);
+	  $t_node->file_link = _preprocess_node_file__field_file($t_node);
+      $node->translations->data[] = $t_node;
+	}
+  }
+} 
+
+function _preprocess_node_file__field_file($node) {
+  // Build link for ECL download 
+  // If file is external
+  if (count($node->field_is_legacy_link) && $node->field_is_legacy_link[LANGUAGE_NONE][0]['value']) {
+	$link = $node->field_legacy_link[LANGUAGE_NONE][0][url];
+  }
+  // If internal and we have a file
+  elseif (count($node->field_file_file)) {
+	$file = $node->field_file_file[LANGUAGE_NONE][0];
+    $link = file_create_url($file['uri']);
+	$size = file_size_human($file['filesize']);
+  }
+  $info = [];
+  if($size) $info[] = $size;
+  if($more_info) $info[] = $more_info;  
+  
+  $info = join (' - ', $info);
+  $info = (strlen($info))?'(' . $info . ')':null;
+  
+  return ['link'=>$link, 'info'=>$info];
+}
+/**
+ * Implements hook_preprocess_hook().
+ */
+function know4pol_ec_europa_preprocess_menu_link__user_menu(&$variables) {
+  $variables['element']['#attributes']['class'] = ['ecl-navigation-list__item'];
+}
+
+/**
+ * Provides a readable filesize text from number;
+ * Author: couneje;
+ */
+
+function file_size_human($size, $i=0, $base=1024) {
+	// iterate per base until it can't be divided.
+	for(;$size > $base; $size /= $base, $i++);
+	// return rounded number and unit
+	return round($size, $i>0?2:0) . ' ' . ["bytes", "KB", "MB", "GB", "TB", "PB", "><"][$i];
 }
---
+++ sites/all/themes/know4pol_ec_europa/templates/node/node--file.tpl.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * @file
+ * Contains template file.
+ * Todo: File info (+field)
+ * Todo: Auto Open available language (system taxonomy)
+ */
+?>
+<div class="content">
+<!-- Translations -->
+<section class="ecl-file ecl-file--translation">
+  <div class="ecl-file__body">
+    <div class="ecl-row">
+      <div class=" ecl-col ecl-col-md-8">
+        <span class="ecl-icon <?php print $variables['elements']['#node']->file_type_css; ?> ecl-file__icon"></span>
+        <div class="ecl-file__metadata">
+          <div class="ecl-file__title"><?php print $variables['elements']['#node']->title; ?></div>
+          <div class="ecl-file__info">
+            <span class="ecl-file__language"><?php print $variables['elements']['language']->title; ?></span> <?php if($variables['elements']['#node']->file_link['info']) print $variables['elements']['#node']->file_link['info']; ?>
+          </div>
+        </div>
+      </div>
+
+      <div class="ecl-col-12 ecl-col-md-4">
+        <?php if($variables['elements']['#node']->file_link):?>
+          <a href="<?php print $variables['elements']['#node']->file_link['link']; ?>"><button class="ecl-button ecl-button--default ecl-button--block ecl-button--file ecl-file__download" type="button"><?php print t('Download'); ?><?php if($variables['elements']['#node']->file_link['info']):?><span class="ecl-u-sr-only"><?php print $variables['elements']['#node']->file_link['info']; ?></span><?php endif; ?></button></a>
+		<?php else: ?>
+		  <button class="ecl-button ecl-button--default ecl-button--block ecl-button--file" type="button"><?php print t('Unavailable'); ?></button>
+		<?php endif; ?>
+      </div>
+    </div>
+  </div>
+  
+  <?php if (count($variables['translations']->data)): ?>
+  <div class="ecl-file__translations">
+	
+    <button class="ecl-button ecl-button--secondary ecl-button--file ecl-file__translations-toggle" aria-controls="translations-expand" aria-expanded="false" id="translations-expand-button" type="button" title="Click to see translations"><?php print t('Available languages' . ' (' . count($variables['translations']->data)) . ')';?></button>
+
+    <div aria-hidden="true" aria-labelledby="translations-expand-button" id="translations-expand">
+      <ul class="ecl-file__translations-list">
+		<?php foreach($variables['translations']->data as $f): ?>
+        <li class="ecl-file__translations-item">
+          <div class="ecl-file__translations-metadata">
+            <span class="ecl-file__translations-title"><?php print $f->title ?></span>
+			<?php if($f->file_link['info']):?>
+              <div class="ecl-file__translations-info"><?php print $f->file_link['info']; ?></div>
+			<?php endif; ?>
+          </div>
+          <?php if($f->file_link):?>
+            <a href="<?php print $f->file_link['link']; ?>"><button class="ecl-button ecl-button--secondary ecl-button--file ecl-file__translations-download" type="button"><?php print t('Download'); ?><?php if($f->file_link['info']):?><span class="ecl-u-sr-only"><?php print $f->file_link['info']; ?></span><?php endif; ?></button></a>
+		  <?php else: ?>
+		    <button class="ecl-button ecl-button--secondary ecl-button--file" type="button"><?php print t('Unavailable'); ?></button>
+		  <?php endif; ?>
+        </li>
+		<?php endforeach; ?>
+      </ul>
+    </div>
+  </div>
+  <?php endif; ?>
+</section>
+
+
+
+
+
+</div>
