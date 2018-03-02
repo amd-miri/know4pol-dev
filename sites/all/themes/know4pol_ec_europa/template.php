34e5635f9b32216a3973534dbb5d12d6db96e18b
Ahmad Miri <amd.miri@gmail.com>
2018-03-02 17:41:26 +0100

    Passed throught PHPCS

--- sites/all/themes/know4pol_ec_europa/template.php
+++ sites/all/themes/know4pol_ec_europa/template.php
@@ -42,110 +42,127 @@ function know4pol_ec_europa_menu_link(array $variables) {
 function know4pol_ec_europa_preprocess_node(&$variables, $hook) {
   // For all content types.
   drupal_add_js('https://visualise.jrc.ec.europa.eu/javascripts/api/viz_v1.js', 'external');
-  // Node is a file
-  if($variables['type'] == 'file') _know4pol_ec_europa_preprocess_node_file($variables, $hook);
-}
-  
-function _know4pol_ec_europa_preprocess_node_file(&$variables, $hook) {
-  $node = $variables['elements']['#node'];
-  // is file a link or a file ?
-  $node->file_link = _know4pol_ec_europa_preprocess_node_file__field_file($node);
-  // filetype icon
-  $_icon_css = $node->field_file_type[LANGUAGE_NONE][0]['taxonomy_term']->field_file_type_classname[LANGUAGE_NONE];
-  // default icon if no type
-  $node->file_type_css = "." . count($_icon_css)?$_icon_css[0][value]:'ecl-icon--file';
-  
-  
-  // languages versions
-  $translations = translation_node_get_translations($node->nid);
-  foreach ($translations as $t) {
-    // other languages olny
-	if($t->nid != $node->nid) {
-	  $t_node = node_load($t->nid);
-	  $t_node->file_link = _know4pol_ec_europa_preprocess_node_file__field_file($t_node);
-      $node->translations->data[] = $t_node;
-	}
+  // Node is a file.
+  if ($variables['type'] == 'file') {
+    _know4pol_ec_europa_preprocess_node_file($variables);
   }
-} 
-
-/**
- * Preprocess fields field_file. Analyse legacy link content and possible more info to display into the ECL block.
- *
- * @param node $node
- * The node.
- *
- * @return array 
- * Parameters of the file (link and infos).
- */
-function _know4pol_ec_europa_preprocess_node_file__field_file($node) {
-  // Build link for ECL download 
-  // If file is external
-  if (count($node->field_is_legacy_link) && $node->field_is_legacy_link[LANGUAGE_NONE][0]['value']) {
-	$link = $node->field_legacy_link[LANGUAGE_NONE][0][url];
-  }
-  // If internal and we have a file
-  elseif (count($node->field_file_file)) {
-	$file = $node->field_file_file[LANGUAGE_NONE][0];
-    $link = file_create_url($file['uri']);
-	$size = _know4pol_ec_europa_file_size_human($file['filesize']);
-  }
-  $info = [];
-  if($size) $info[] = $size;
-  if($more_info) $info[] = $more_info;  
-  
-  $info = join (' - ', $info);
-  $info = (strlen($info))?'(' . $info . ')':null;
-  
-  return ['link'=>$link, 'info'=>$info];
 }
 
-
 /**
- * Preprocess field data visualisation urls
+ * Preprocess node_file.
+ *
+ * Analyse legacy link content
+ * and possible more info to display into the ECL block.
  *
  * @param array $variables
- * Variables from the original hook.
+ *   Variables from the original hook.
  */
-function _know4pol_ec_europa_preprocess_field__field_vis_data_url__visualisation(&$variables) {
-  
-  $el = &$variables['items'][0]['#element'];
-  $el['v_type'] = $variables['element']['#object']->field_vis_type[LANGUAGE_NONE][0]['value'];
-  
-  // build data paramters for the visualisation types
-  switch($el['v_type']) {
-    case "Tableau":
-	  $matches = [];
-	  if( preg_match('/(.*?ec\.europa\.eu)(\/t\/.*?)\/.*?\/([^\?]+)/', $el['url'], $matches)) {
-	    $el['thost'] = urlencode($matches[1] . '/');
-		$el['troot'] = htmlentities($matches[2]);
-		$el['tname'] = htmlentities($matches[3]);
-	  }
-	  break;
-	case "Highcharts":
-	  // no preprocess
-	break;
+function _know4pol_ec_europa_preprocess_node_file(&$variables) {
+  $node = $variables['elements']['#node'];
+  // Is file a link or a file ?
+  $node->file_link = _know4pol_ec_europa_preprocess_node_file__field_file($node);
+  // Filetype icon.
+  $_icon_css = $node->field_file_type[LANGUAGE_NONE][0]['taxonomy_term']->field_file_type_classname[LANGUAGE_NONE];
+  // Default icon if no type.
+  $node->file_type_css = "." . count($_icon_css) ? $_icon_css[0][value] : 'ecl-icon--file';
+
+  // Languages versions.
+  $translations = translation_node_get_translations($node->nid);
+  foreach ($translations as $t) {
+    // Other languages olny.
+    if ($t->nid != $node->nid) {
+      $t_node = node_load($t->nid);
+      $t_node->file_link = _know4pol_ec_europa_preprocess_node_file__field_file($t_node);
+      $node->translations->data[] = $t_node;
+    }
   }
 }
 
 /**
- * Provides a readable filesize text from a number by reccurently dividing by a base.
+ * Preprocess fields field_file.
+ *
+ * Analyse legacy link content
+ * and possible more info to display into the ECL block.
+ *
+ * @param node $node
+ *   The node.
+ *
+ * @return array
+ *   Parameters of the file (link and infos).
+ */
+function _know4pol_ec_europa_preprocess_node_file__field_file($node) {
+  // Build link for ECL download
+  // If file is external.
+  if (count($node->field_is_legacy_link) && $node->field_is_legacy_link[LANGUAGE_NONE][0]['value']) {
+    $link = $node->field_legacy_link[LANGUAGE_NONE][0][url];
+  }
+  // If internal and we have a file.
+  elseif (count($node->field_file_file)) {
+    $file = $node->field_file_file[LANGUAGE_NONE][0];
+    $link = file_create_url($file['uri']);
+    $size = _know4pol_ec_europa_file_size_human($file['filesize']);
+  }
+  $info = array();
+  if ($size) {
+    $info[] = $size;
+  }
+
+  $info = implode(' - ', $info);
+  $info = (strlen($info)) ? '(' . $info . ')' : NULL;
+
+  return array('link' => $link, 'info' => $info);
+}
+
+/**
+ * Preprocess field data visualisation urls.
+ *
+ * @param array $variables
+ *   Variables from the original hook.
+ */
+function _know4pol_ec_europa_preprocess_field__field_vis_data_url__visualisation(&$variables) {
+
+  $el = &$variables['items'][0]['#element'];
+  $el['v_type'] = $variables['element']['#object']->field_vis_type[LANGUAGE_NONE][0]['value'];
+
+  // Build data paramters for the visualisation types.
+  switch ($el['v_type']) {
+    case "Tableau":
+      $matches = array();
+      if (preg_match('/(.*?ec\.europa\.eu)(\/t\/.*?)\/.*?\/([^\?]+)/', $el['url'], $matches)) {
+        $el['thost'] = urlencode($matches[1] . '/');
+        $el['troot'] = htmlentities($matches[2]);
+        $el['tname'] = htmlentities($matches[3]);
+      }
+      break;
+
+    case "Highcharts":
+      // No preprocess.
+      break;
+  }
+}
+
+/**
+ * Provides a readable filesize text.
+ *
+ * From a number by reccurently dividing by a base.
  *
  * @param int $size
- * The filesize in bytes.
- *
+ *   The filesize in bytes.
  * @param int $i
- * Default 0, can be used for overriding the start of the division if $size is already divided.
- *
+ *   Default 0, can be used for overriding
+ *   the start of the division if $size is already divided.
  * @param int $base
- * Default 1024, the divider for next unit.
+ *   Default 1024, the divider for next unit.
  *
- * @return string 
- * A string representing $size with the biggest unit possible. 
+ * @return string
+ *   A string representing $size with the biggest unit possible.
  */
-
-function _know4pol_ec_europa_file_size_human($size, $i=0, $base=1024) {
-	// iterate per base until it can't be divided anymore.
-	for(;$size > $base; $size /= $base, $i++);
-	// return rounded number and unit.
-	return round($size, $i>0?2:0) . ' ' . ["bytes", "KB", "MB", "GB", "TB", "PB", "><"][$i];
+function _know4pol_ec_europa_file_size_human($size, $i = 0, $base = 1024) {
+  // Iterate per base until it can't be divided anymore.
+  for (; $size > $base; $i++) {
+    $size /= $base;
+  }
+  // Return rounded number and unit.
+  return round($size, $i > 0 ? 2 : 0) . ' ' .
+    ["bytes", "KB", "MB", "GB", "TB", "PB", "><"][$i];
 }
--- sites/all/themes/know4pol_ec_europa/templates/fields/field--field-vis-data-url.tpl.php
+++ sites/all/themes/know4pol_ec_europa/templates/fields/field--field-vis-data-url.tpl.php
@@ -2,41 +2,39 @@
 
 /**
  * @file
- * Contains template file.
+ * Template for field-vis-data-url.
  */
 ?>
 
 <div<?php print $atomium['attributes']['wrapper']; ?>>
-    <div class="ecl-field__label"><?php print $label ?></div>
-    <div class="ecl-field__body">
-      <?php foreach ($items as $delta => $item): ?>
-			
-          <div<?php print $atomium['attributes'][$delta]; ?>>
-		  <?php if ($item['#element']['v_type'] == "Tableau"): ?>
-		  <div class='tableauPlaceholder' style='width: 100%; height: 724px;'>
-			<object class='tableauViz' width='100%' height='724' style='display:none;'>
-				<param name='host_url' value='<?php print $item['#element']['thost']; ?>' /> 
-				<param name='site_root' value='<?php print $item['#element']['troot']; ?>' />
-				<param name='name' value='<?php print $item['#element']['tname']; ?>' />
-				<param name='embed_code_version' value='3' /> 
-				<param name='tabs' value='no' />
-				<param name='toolbar' value='yes' />
-				<param name='showAppBanner' value='false' />
-			</object>
-		  </div>
-		  <?php elseif ($item['#element']['v_type']=="Highcharts"): ?>
-			<div id="chart_container_global" style="background-color: #eeeeee; padding: 1em;">
-			  <div id="chart_container"></div>
-			  <div id="chart_data_set" style="text-align: center; padding-top: 1em;"></div>
-			</div>
-			<script type="application/json">{ 
-			  "service": "charts",
-			  "provider": "highcharts",
-			  "custom": "<?php print $item['#element']['url']; ?>"
-			}</script>
-			<script defer src="//europa.eu/webtools/load.js" type="text/javascript"></script>
-		  <?php endif; ?>
-      <?php endforeach; ?>
-    </div>
-		
+  <div class="ecl-field__label"><?php print $label ?></div>
+  <div class="ecl-field__body">
+    <?php foreach ($items as $delta => $item): ?>
+      <div<?php print $atomium['attributes'][$delta]; ?>>
+      <?php if ($item['#element']['v_type'] == "Tableau"): ?>
+        <div class='tableauPlaceholder' style='width: 100%; height: 724px;'>
+          <object class='tableauViz' width='100%' height='724' style='display:none;'>
+            <param name='host_url' value='<?php print $item['#element']['thost']; ?>' /> 
+            <param name='site_root' value='<?php print $item['#element']['troot']; ?>' />
+            <param name='name' value='<?php print $item['#element']['tname']; ?>' />
+            <param name='embed_code_version' value='3' /> 
+            <param name='tabs' value='no' />
+            <param name='toolbar' value='yes' />
+            <param name='showAppBanner' value='false' />
+          </object>
+        </div>
+        <?php elseif ($item['#element']['v_type'] == "Highcharts"): ?>
+        <div id="chart_container_global" style="background-color: #eeeeee; padding: 1em;">
+          <div id="chart_container"></div>
+          <div id="chart_data_set" style="text-align: center; padding-top: 1em;"></div>
+        </div>
+        <script type="application/json">{ 
+          "service": "charts",
+          "provider": "highcharts",
+          "custom": "<?php print $item['#element']['url']; ?>"
+        }</script>
+        <script defer src="//europa.eu/webtools/load.js" type="text/javascript"></script>
+        <?php endif; ?>
+    <?php endforeach; ?>
+  </div>
 </div>
