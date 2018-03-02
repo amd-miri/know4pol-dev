372e20bf59fcfd54e2ceebe8424a33bffb6230af
COUNET Jerome (JRC-EXT) <Jerome.COUNET@ext.ec.europa.eu>
2018-03-02 14:43:34 +0100

    Added field template for visualisation & file ECL

--- sites/all/themes/know4pol_ec_europa/template.php
+++ sites/all/themes/know4pol_ec_europa/template.php
@@ -42,18 +42,14 @@ function know4pol_ec_europa_menu_link(array $variables) {
 function know4pol_ec_europa_preprocess_node(&$variables, $hook) {
   // For all content types.
   drupal_add_js('https://visualise.jrc.ec.europa.eu/javascripts/api/viz_v1.js', 'external');
-  //drupal_add_js(path_to_theme() . '/assets/scripts/ecl-navigation-inpages.js', array('group' => JS_THEME));
-
-  /* COUNEJE */
-
   // Node is a file
-  if($variables['type'] == 'file') _preprocess_node_file($variables, $hook);
+  if($variables['type'] == 'file') _know4pol_ec_europa_preprocess_node_file($variables, $hook);
 }
   
-function _preprocess_node_file(&$variables, $hook) {
+function _know4pol_ec_europa_preprocess_node_file(&$variables, $hook) {
   $node = $variables['elements']['#node'];
   // is file a link or a file ?
-  $node->file_link = _preprocess_node_file__field_file($node);
+  $node->file_link = _know4pol_ec_europa_preprocess_node_file__field_file($node);
   // filetype icon
   $_icon_css = $node->field_file_type[LANGUAGE_NONE][0]['taxonomy_term']->field_file_type_classname[LANGUAGE_NONE];
   // default icon if no type
@@ -66,13 +62,22 @@ function _preprocess_node_file(&$variables, $hook) {
     // other languages olny
 	if($t->nid != $node->nid) {
 	  $t_node = node_load($t->nid);
-	  $t_node->file_link = _preprocess_node_file__field_file($t_node);
+	  $t_node->file_link = _know4pol_ec_europa_preprocess_node_file__field_file($t_node);
       $node->translations->data[] = $t_node;
 	}
   }
 } 
 
-function _preprocess_node_file__field_file($node) {
+/**
+ * Preprocess fields field_file. Analyse legacy link content and possible more info to display into the ECL block.
+ *
+ * @param node $node
+ * The node.
+ *
+ * @return array 
+ * Parameters of the file (link and infos).
+ */
+function _know4pol_ec_europa_preprocess_node_file__field_file($node) {
   // Build link for ECL download 
   // If file is external
   if (count($node->field_is_legacy_link) && $node->field_is_legacy_link[LANGUAGE_NONE][0]['value']) {
@@ -82,7 +87,7 @@ function _preprocess_node_file__field_file($node) {
   elseif (count($node->field_file_file)) {
 	$file = $node->field_file_file[LANGUAGE_NONE][0];
     $link = file_create_url($file['uri']);
-	$size = file_size_human($file['filesize']);
+	$size = _know4pol_ec_europa_file_size_human($file['filesize']);
   }
   $info = [];
   if($size) $info[] = $size;
@@ -93,21 +98,54 @@ function _preprocess_node_file__field_file($node) {
   
   return ['link'=>$link, 'info'=>$info];
 }
+
+
 /**
- * Implements hook_preprocess_hook().
+ * Preprocess field data visualisation urls
+ *
+ * @param array $variables
+ * Variables from the original hook.
  */
-function know4pol_ec_europa_preprocess_menu_link__user_menu(&$variables) {
-  $variables['element']['#attributes']['class'] = ['ecl-navigation-list__item'];
+function _know4pol_ec_europa_preprocess_field__field_vis_data_url__visualisation(&$variables) {
+  
+  $el = &$variables['items'][0]['#element'];
+  $el['v_type'] = $variables['element']['#object']->field_vis_type[LANGUAGE_NONE][0]['value'];
+  
+  // build data paramters for the visualisation types
+  switch($el['v_type']) {
+    case "Tableau":
+	  $matches = [];
+	  if( preg_match('/(.*?ec\.europa\.eu)(\/t\/.*?)\/.*?\/([^\?]+)/', $el['url'], $matches)) {
+	    $el['thost'] = urlencode($matches[1] . '/');
+		$el['troot'] = htmlentities($matches[2]);
+		$el['tname'] = htmlentities($matches[3]);
+	  }
+	  break;
+	case "Highcharts":
+	  // no preprocess
+	break;
+  }
 }
 
 /**
- * Provides a readable filesize text from number;
- * Author: couneje;
+ * Provides a readable filesize text from a number by reccurently dividing by a base.
+ *
+ * @param int $size
+ * The filesize in bytes.
+ *
+ * @param int $i
+ * Default 0, can be used for overriding the start of the division if $size is already divided.
+ *
+ * @param int $base
+ * Default 1024, the divider for next unit.
+ *
+ * @return string 
+ * A string representing $size with the biggest unit possible. 
  */
 
-function file_size_human($size, $i=0, $base=1024) {
-	// iterate per base until it can't be divided.
+function _know4pol_ec_europa_file_size_human($size, $i=0, $base=1024) {
+	// iterate per base until it can't be divided anymore.
 	for(;$size > $base; $size /= $base, $i++);
-	// return rounded number and unit
+	// return rounded number and unit.
 	return round($size, $i>0?2:0) . ' ' . ["bytes", "KB", "MB", "GB", "TB", "PB", "><"][$i];
 }
