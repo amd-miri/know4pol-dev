5f62f3bb9aa04d99312bdd2717f6375b22ba11ef
Ahmad Miri <amd.miri@gmail.com>
2018-07-02 16:48:45 +0200

    phpcs fix

--- sites/all/modules/custom/know4pol_core/plugins/ctabutton/dialogs/ctabutton.js
+++ sites/all/modules/custom/know4pol_core/plugins/ctabutton/dialogs/ctabutton.js
@@ -22,18 +22,18 @@ CKEDITOR.dialog.add('ctabuttonDialog', function (editor) {
               commit: function (element) {
               }
             },
-           {
+            {
               type: 'select',
               id: 'protocol',
               label: 'Protocol',
-              items: [['<other>',''],['http://'], ['https://'],['mailto:'],['ftp://'],['news://']],
-              'default': '<other>',
-                setup: function (element) {
-                },
-                commit: function (element) {
-                }
-           },
-           {
+              items: [['<other>','<other>'], ['http://','http://'], ['https://','https://'], ['mailto:'], ['ftp://'], ['news://']],
+              default: '<other>',
+              setup: function (element) {
+              },
+              commit: function (element) {
+              }
+            },
+            {
               type: 'text',
               id: 'enterurl',
               label: 'Enter URL',
@@ -43,60 +43,52 @@ CKEDITOR.dialog.add('ctabuttonDialog', function (editor) {
               commit: function (element) {
               }
             }
-                ]
+          ]
         }
-        ],
+      ],
 
-        // Invoked when the dialog is loaded.
-        onShow: function () {
-
-            // Get the selection from the editor.
-            var selection = editor.getSelection();
-
-            // Get the element at the start of the selection.
-            var element = selection.getStartElement();
-
-            // Get the <abbr> element closest to the selection, if it exists.
-            if (element) {
-                element = element.getAscendant('a', true);
-            }
-
-             // Create a new <a> element if it does not exist.
-            if (!element || element.getName() != 'a') {
-                element = editor.document.createElement('a');
-
-                // Flag the insertion mode for later use.
-                this.insertMode = true;
-            }
-            else {
-                this.insertMode = false;
-            }
-
-            // Store the reference to the <abbr> element in an internal property, for later use.
-              this.element = element;
-
-            // Invoke the setup methods of all dialog window elements, so they can load the element attributes.
-            if (!this.insertMode) {
-                this.setupContent(this.element);
-            }
-        },
-
-        onOk: function () {
-            var textObj = this.getContentElement('tab-external', 'txtchng');
-            var enterurlObj = this.getContentElement('tab-external', 'enterurl');
-            var protocolObj = this.getContentElement('tab-external', 'protocol');
-            var text = textObj.getValue();
-            var enterurl = enterurlObj.getValue();
-            var protocol = protocolObj.getValue();
-            this.element.setHtml('<a href="' + protocol + '' + enterurl + '" class="ecl-button ecl-button--call ecl-button--caret-right">' + text + '</button>');
-            var txt = this.element;
-            this.commitContent(txt);
-              if (this.insertMode) {
-                editor.insertElement(txt);
-              }
-            var range = editor.createRange();
-            range.moveToPosition(range.root, CKEDITOR.POSITION_BEFORE_END);
-            editor.getSelection().selectRanges([range]);
+      // Invoked when the dialog is loaded.
+      onShow: function () {
+        // Get the selection from the editor.
+        var selection = editor.getSelection();
+        // Get the element at the start of the selection.
+        var element = selection.getStartElement();
+        // Get the <abbr> element closest to the selection, if it exists.
+        if (element) {
+          element = element.getAscendant('a', true);
         }
+        // Create a new <a> element if it does not exist.
+        if (!element || element.getName() != 'a') {
+          element = editor.document.createElement('a');
+          // Flag the insertion mode for later use.
+          this.insertMode = true;
+        }
+        else {
+          this.insertMode = false;
+        }
+        // Store the reference to the <abbr> element in an internal property, for later use.
+        this.element = element;
+        // Invoke the setup methods of all dialog window elements, so they can load the element attributes.
+        if (!this.insertMode) {
+         this.setupContent(this.element);
+        }
+      },
+      onOk: function () {
+        var textObj = this.getContentElement('tab-external', 'txtchng');
+        var enterurlObj = this.getContentElement('tab-external', 'enterurl');
+        var protocolObj = this.getContentElement('tab-external', 'protocol');
+        var text = textObj.getValue();
+        var enterurl = enterurlObj.getValue();
+        var protocol = protocolObj.getValue();
+        this.element.setHtml('<a href="' + protocol + '' + enterurl + '" class="ecl-button ecl-button--call ecl-button--caret-right">' + text + '</button>');
+        var txt = this.element;
+        this.commitContent(txt);
+        if (this.insertMode) {
+          editor.insertElement(txt);
+        }
+        var range = editor.createRange();
+        range.moveToPosition(range.root, CKEDITOR.POSITION_BEFORE_END);
+        editor.getSelection().selectRanges([range]);
+       }
     };
 });
