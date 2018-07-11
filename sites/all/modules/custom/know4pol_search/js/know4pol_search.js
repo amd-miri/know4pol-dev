668be3fb84be4d3a44aacf1a4a626d4b7f54fe36
Ahmad Miri <amd.miri@gmail.com>
2018-07-11 15:15:28 +0200

    Fix the dropdown css

--- sites/all/modules/custom/know4pol_search/js/know4pol_search.js
+++ sites/all/modules/custom/know4pol_search/js/know4pol_search.js
@@ -12,12 +12,22 @@
           $("#countries-dropdown-list").slideUp();
           $("button#country_button").html('Countries ▼');
           $("#topics-dropdown-list").slideUp();
+          // Change CSS of both buttons.
+          $(this).css("background-color", "white");
+          $(this).css("color", "#004494");
+          $("button#country_button").css("background-color", "white");
+          $("button#country_button").css("color", "#004494");
         }
         else {
           $(this).html('Topics ▲');
           $("#topics-dropdown-list").slideDown();
           $("button#country_button").html('Countries ▼');
           $("#countries-dropdown-list").slideUp();
+          // Change CSS of both buttons.
+          $(this).css("background-color", "#004494");
+          $(this).css("color", "white");
+          $("button#country_button").css("background-color", "white");
+          $("button#country_button").css("color", "#004494");
         }
       });
       $("button#country_button").click(function () {
@@ -26,12 +36,22 @@
           $("#topics-dropdown-list").slideUp();
           $("button#topic_button").html('Topics ▼');
           $("#countries-dropdown-list").slideUp();
+          // Change CSS of both buttons.
+          $(this).css("background-color", "white");
+          $(this).css("color", "#004494");
+          $("button#topic_button").css("background-color", "white");
+          $("button#topic_button").css("color", "#004494");
         }
         else {
           $(this).html('Countries ▲');
           $("#countries-dropdown-list").slideDown();
           $("button#topic_button").html('Topics ▼');
           $("#topics-dropdown-list").slideUp();
+          // Change CSS of both buttons.
+          $(this).css("background-color", "#004494");
+          $(this).css("color", "white");
+          $("button#topic_button").css("background-color", "white");
+          $("button#topic_button").css("color", "#004494");
         }
       });
     }
