581f014cef2ae1874adb3b865f7da24657b0270b
ARINZE <arinze@ARINZEs-MacBook-Pro.local>
2018-07-20 09:37:50 +0200

    footer template modified

--- sites/all/themes/know4pol_ec_europa/templates/footer/footer.tpl.php
+++ sites/all/themes/know4pol_ec_europa/templates/footer/footer.tpl.php
@@ -15,9 +15,24 @@
         </div>
         <div class="ecl-col-sm ecl-footer__column">
           <?php print render($footer_middle); ?>
+            
+            <p class="ecl-footer__label">Follow us:</p>
+            
+            <ul class="ecl-footer__menu ecl-list--inline ecl-footer__social-links">
+            <li class="ecl-footer__menu-item">
+ 
+                <a class="ecl-link ecl-footer__link" href="<?php print 'http://'. $twitter;?>"><span class="ecl-icon ecl-icon--twitter ecl-footer__social-icon"></span>Twitter</a>
+            </li>
+            <li class="ecl-footer__menu-item">
+ 
+              <a class="ecl-link ecl-link--external ecl-footer__link" href="<?php print 'http://'. $blog;?>">Other social media</a>
+            </li>
+          </ul>
+            
         </div>
         <div class="ecl-col-sm ecl-footer__column">
           <?php print render($footer_right); ?>
+            <a class="ecl-footer__link ecl-link ecl-link--inverted" href="<?php print $about_page; ?>"> <?php if(isset($group_title)){ print 'About'. $group_title;} ?></a>
         </div>
       </div>
     </div>
