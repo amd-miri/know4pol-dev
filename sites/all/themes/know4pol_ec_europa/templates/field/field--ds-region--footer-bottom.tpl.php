dccaeb1a8d75e20e117577ef70cdac8a7a7c669d
Ahmad Miri <amd.miri@gmail.com>
2018-08-27 16:10:23 +0200

    minor changes

--- sites/all/themes/know4pol_ec_europa/templates/field/field--ds-region--footer-bottom.tpl.php
+++ sites/all/themes/know4pol_ec_europa/templates/field/field--ds-region--footer-bottom.tpl.php
@@ -6,7 +6,7 @@
  */
 ?>
 <tr>
-    <td><?php print $label ?></td>
+    <td><?php print $label; ?></td>
     <td<?php print $atomium['attributes']['wrapper']; ?>>
       <?php foreach ($items as $delta => $item) : ?>
           <div<?php print $atomium['attributes'][$delta]; ?>><?php print render($item); ?></div>
