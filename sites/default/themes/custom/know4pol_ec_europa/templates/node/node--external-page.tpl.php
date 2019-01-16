9f9d84a40a34f3687e54cc0d536190748de6e600
COUNET Jerome (JRC-EXT) <Jerome.COUNET@ext.ec.europa.eu>
2019-01-16 15:21:01 +0100

    Alternative to WYSIWYG iframes CT + validation

---
+++ sites/default/themes/custom/know4pol_ec_europa/templates/node/node--external-page.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Template for the File content type.
+ */
+?>
+
+<iframe 
+  src="<?php print($variables['field_ext_url'][0]['url']); ?>" 
+  height="<?php print($variables['field_frame_height'][0]['value']); ?>" 
+  width="<?php print($variables['field_frame_width'][0]['value']); ?>" 
+  style="border:none;">
+</iframe>
