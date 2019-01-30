70467e472bcb343916d563ab6a83cee740c55490
COUNET Jerome (JRC-EXT) <Jerome.COUNET@ext.ec.europa.eu>
2019-01-30 16:49:29 +0100

    Templates chatbox

--- sites/default/themes/custom/know4pol_ec_europa/templates/views/views-view-unformatted--chatfuel-rss--page--page-1.tpl.php
+++ sites/default/themes/custom/know4pol_ec_europa/templates/views/views-view-unformatted--chatfuel-rss--page--page-1.tpl.php
@@ -7,6 +7,10 @@
  * @ingroup views_templates
  */
 ?>
+<?php $i = 0; ?>
 <?php foreach ($rows as $id => $row): ?>
     <?php print $row; ?>
+    <?php if(++$i < count($rows)): ?>
+      ,
+    <?php endif; ?>
 <?php endforeach; ?>
