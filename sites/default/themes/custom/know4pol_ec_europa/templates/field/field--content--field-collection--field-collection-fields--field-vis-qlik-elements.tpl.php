ece6005354f7ebeb485fef07132c02280b42e757
COUNET Jerome (JRC-EXT) <Jerome.COUNET@ext.ec.europa.eu>
2019-07-24 16:34:00 +0200

    Fixes for release v .56 in reference

--- sites/default/themes/custom/know4pol_ec_europa/templates/field/field--content--field-collection--field-collection-fields--field-vis-qlik-elements.tpl.php
+++ sites/default/themes/custom/know4pol_ec_europa/templates/field/field--content--field-collection--field-collection-fields--field-vis-qlik-elements.tpl.php
@@ -8,7 +8,7 @@
 <div<?php print $atomium['attributes']['wrapper']; ?> style="width:100%">
   <div class="ecl-field__label"><?php print $label ?></div>
   <div class="ecl-field__body">
-    <?php print render($html_layout); ?>
+    <?php print $html_layout; ?>
   </div>
    <script>
       var config = {
