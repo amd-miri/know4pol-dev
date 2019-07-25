1f88562da576b1826ae6d0a24c281e36334cda5b
COUNET Jerome (JRC-EXT) <Jerome.COUNET@ext.ec.europa.eu>
2019-07-25 13:48:02 +0200

    Fix js not in template, pass variables via settings

---
+++ sites/default/themes/custom/know4pol_ec_europa/assets/scripts/qap-k4p-config.js
@@ -0,0 +1,30 @@
+/**
+ * @file
+ * JS file for Qlik visualisation via QAP.
+ */
+
+var config = {
+  host: 'qap-datam.jrc.ec.europa.eu',
+  prefix: '/qap/',
+  port: 443,
+  isSecure: true
+}
+
+config.identity = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+var baserUrl = (config.isSecure ? 'https' : 'http') + '://' + config.host + config.prefix;
+require.config({
+  baseUrl: baserUrl + 'resources'
+});
+
+require(['js/qlik'], function (qlik) {
+  qlik.setOnError(function (err) {console.log(err); alert(err.message); });
+
+  var app = qlik.openApp(Drupal.settings.know4pol.qapid, config);
+  for (i = 0; i < Drupal.settings.know4pol.items.length; i++) {
+    app.getObject(
+      Drupal.settings.know4pol.items[i].html_id,
+      Drupal.settings.know4pol.items[i].id,
+      Drupal.settings.know4pol.items[i].option
+    );
+  }
+});
--- sites/default/themes/custom/know4pol_ec_europa/templates/field/field--content--field-collection--field-collection-fields--field-vis-qlik-elements.tpl.php
+++ sites/default/themes/custom/know4pol_ec_europa/templates/field/field--content--field-collection--field-collection-fields--field-vis-qlik-elements.tpl.php
@@ -10,32 +10,4 @@
   <div class="ecl-field__body">
     <?php print render($html_layout); ?>
   </div>
-   <script>
-      var config = {
-             host: 'qap-datam.jrc.ec.europa.eu',
-             prefix: '/qap/',
-             port: 443,
-             isSecure: true
-      }
-
-      config.identity = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-      var baserUrl = (config.isSecure ? 'https' : 'http') + '://' + config.host + config.prefix;
-      require.config( {
-                     baseUrl: baserUrl + 'resources'
-              } );
-              
-      require( ["js/qlik"], function( qlik ) {
-         qlik.setOnError( function(err) {console.log(err); alert(err.message); });
-                     
-      var app = qlik.openApp("<?php print $element['#object']->field_vis_qlik_app_id[LANGUAGE_NONE][0]['value'] ?>", config);
-       
-      <?php foreach ($items as $delta => $item): ?>
-        app.getObject("<?php print $item['html_id']; ?>", 
-                      "<?php print $item['id']; ?>" 
-                      <?php if(isset($item['option'])): ?>, <?php print $item['option']; ?><?php
-                      endif; ?>
-                     ); 
-      <?php endforeach; ?>
-      });
-   </script>
 </div>
